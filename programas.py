1356c34e0ef9be0bf2b9afae0af67ec0e554b76d
JhuniorCapa <135991749+JhuniorCapa@users.noreply.github.com>
2023-06-15 14:55:49 +0000

    cambios programa

---
+++ programas.py
@@ -0,0 +1,7 @@
+from matematica import sumar, restar, multiplicar, division
+from algebra import distancia
+print(restar(4,2))
+print(sumar(2,3))
+print(multiplicar(4,2))
+print(division(2,3))
+print(distancia(1,2,3,4))
